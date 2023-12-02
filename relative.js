ced5d2fd382c30b097cb20071abec29095cf3bc0
ashikasurathkal <151603121+ashikasurathkal@users.noreply.github.com>
2023-12-02 12:51:33 +0530

    Add files via upload

---
+++ relative.js
@@ -0,0 +1,13 @@
+gsap.from("jack.ji",{
+scale:0,
+X:100,
+opacity:0,
+duration:2,
+stagger:0.3,
+scrollTrigger:{
+    trigger:"jack.ji",
+    scroller:"body",
+    markers:true,
+    start:"top 70%"
+}
+})
